6b2c64e8c09d14c8c1b1ade8c0168a9685e58ed4
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-09-03 22:47:51 -0600

    adding unit tests

--- testings.py
+++ testings.py
@@ -1,7 +1,18 @@
 import main_file as source
 
+
 def test_get_square_area_1():
     assert source.get_square_area(3) == 9
 
+
 def test_get_square_area_2():
     assert source.get_square_area(7) == 49
+
+
+def test_get_square_perimeter_1():
+    assert source.get_square_perimeter(3) == 12
+
+
+def test_get_square_perimeter_2():
+    assert source.get_square_perimeter(7) == 28
+   
